0f21fe532aa07de1a00156fb100ae8be3c3d5c35
Mevlut Arslan <mvlcfr@Mevluts-MacBook-Pro.local>
2021-01-20 08:02:31 +0100

    Finished the chapter with accuracy sub-chapter

--- chapter-5/cross-entropy.py
+++ chapter-5/cross-entropy.py
@@ -1,11 +1,21 @@
-import math
+import numpy as np
 
-softmax_output = [0.7, 0.1, 0.2]
+class Loss_CategoricalCrossentropy(Loss): # Forward pass
 
-target_output = [1, 0, 0]
-
-loss = - (target_output[0] * math.log(softmax_output[0]) +
-          target_output[1] * math.log(softmax_output[1]) + 
-          target_output[2] * math.log(softmax_output[2]))
-
-print(loss)
+    def forward(self, y_pred, y_true): # Number of samples in a batch
+        samples = len(y_pred)
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+        # Probabilities for target values -
+        # only if categorical labels
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[
+            range(samples), y_true ]
+          # Mask values - only for one-hot encoded labels
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(
+            y_pred_clipped*y_true, axis=1 )
+        # Losses
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
