43c3d42e25b055264ea81a6ef3539af7fb50205c
Mevlut Arslan <MevlutArslan@users.noreply.github.com>
2021-01-30 11:57:43 +0100

    added backwards propagation method to the class

--- relu_activation_func.py
+++ relu_activation_func.py
@@ -1,12 +1,18 @@
 import numpy as np
 
-
 class ReLUActivation():
 
     def forward(self, inputs):
+        # we store inputs to use in backward propagation
+        self.inputs = inputs
         # Calculate output values from input
         '''
             The ReLU activation function outputs 0 for all inputs thats less than 0 and
             it outputs the input for all values greater or equal to zero.
         '''
         self.output = np.maximum(0, inputs)
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+
+        self.dinputs[self.inputs <= 0] = 0
