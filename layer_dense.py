ac3b036b7ede795b075146e2d323dc6d4a021647
Mevlut Arslan <MevlutArslan@users.noreply.github.com>
2021-01-30 11:55:31 +0100

    added method for backwards propagation

--- layer_dense.py
+++ layer_dense.py
@@ -13,5 +13,17 @@ class Layer_Dense:
         # Forward pass
 
     def forward(self, inputs):
+        # we store cause : 
+        # we’ll need them when calculating the partial derivative with respect to weights during backpropagation
+        self.inputs = inputs 
         self.output = np.dot(inputs, self.weights) + self.biases
 
+    # method to calculate the gradient,
+    # takes in the derivates from the previous step in back propagation
+    def backward(self, dvalues):
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+
+        self.dinputs = np.dot(dvalues, self.weights.T)
+
+        
