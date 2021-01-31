31b4e3b34312e8c26b41ae9ef7395c5d0501f820
Mevlut Arslan <MevlutArslan@users.noreply.github.com>
2021-01-31 18:41:29 +0100

    addded backward propagation to  cross-entropy

--- cross_entropy.py
+++ cross_entropy.py
@@ -1,9 +1,10 @@
 import numpy as np
 from loss_class import Loss
 
-class Loss_CategoricalCrossentropy(Loss): # Forward pass
 
-    def forward(self, y_pred, y_true): # Number of samples in a batch
+class Loss_CategoricalCrossentropy(Loss):  # Forward pass
+
+    def forward(self, y_pred, y_true):  # Number of samples in a batch
         samples = len(y_pred)
         # Clip data to prevent division by 0
         # Clip both sides to not drag mean towards any value
@@ -12,11 +13,25 @@ class Loss_CategoricalCrossentropy(Loss): # Forward pass
         # only if categorical labels
         if len(y_true.shape) == 1:
             correct_confidences = y_pred_clipped[
-            range(samples), y_true ]
+                range(samples), y_true]
           # Mask values - only for one-hot encoded labels
         elif len(y_true.shape) == 2:
             correct_confidences = np.sum(
-            y_pred_clipped*y_true, axis=1 )
+                y_pred_clipped*y_true, axis=1)
         # Losses
         negative_log_likelihoods = -np.log(correct_confidences)
         return negative_log_likelihoods
+
+    def backward(self, ground_truth_v, predicted_values_v):
+
+        sample_count = len(predicted_values_v)
+
+        label_count = len(predicted_values_v[0])
+
+        # If labels are sparse, turn them into one-hot vector
+        if len(ground_truth_v.shape) == 1:
+            y_true = np.eye(label_count)[y_true]
+
+        self.dinputs = -ground_truth_v / predicted_values_v
+
+        self.dinputs = self.dinputs / sample_count
