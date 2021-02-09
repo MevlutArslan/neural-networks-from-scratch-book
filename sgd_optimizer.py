95b5d939f2d9ebdcaa27d5fefad9bfb977cd815e
Mevlut Arslan <MevlutArslan@users.noreply.github.com>
2021-02-09 06:00:19 +0100

    implemented momentum to the optimizer

--- sgd_optimizer.py
+++ sgd_optimizer.py
@@ -2,11 +2,12 @@ import numpy as np
 
 
 class SGD_Optimizer():
-    def __init__(self, learning_rate=1.0, decay=0.0):
+    def __init__(self, learning_rate=1.0, decay=0.0, momentum=0.):
         self.learning_rate = learning_rate
         self.current_learning_rate = learning_rate
         self.decay = decay
         self.iterations = 0
+        self.momentum = momentum
 
     def pre_update_params(self):
         if self.decay:
@@ -14,8 +15,33 @@ class SGD_Optimizer():
                 (1 / (1 + self.decay * self.iterations))
 
     def update_parameters(self, layer):
-        layer.weights += -self.learning_rate * layer.dweights
-        layer.biases += -self.learning_rate * layer.dbiases
+        if self.momentum:
+            # If layer does not contain momentum arrays, create them
+            # filled with zeros
+            if not hasattr(layer, 'weight_momentums'):
+                layer.weight_momentums = np.zeros_like(layer.weights)
+                # If there is no momentum array for weights
+                # The array doesn't exist for biases yet either.
+                layer.bias_momentums = np.zeros_like(layer.biases)
+
+            weight_updates = \
+                self.momentum * layer.weight_momentums - \
+                self.current_learning_rate * layer.dweights
+
+            layer.weight_momentums = weight_updates
+
+            bias_updates = \
+                self.momentum * layer.bias_momentums - \
+                self.current_learning_rate * layer.dbiases
+            layer.bias_momentums = bias_updates
+
+        # Vanilla SGD updates (as before momentum update)
+        else:
+            weight_updates = -self.current_learning_rate * layer.dweights
+            bias_updates = -self.current_learning_rate * layer.dbiases
+
+        layer.weights += weight_updates
+        layer.biases += bias_updates
 
     def post_update_params(self):
         self.iterations += 1
