cda5aa69ae9e7fc601f0e79e9347ab5143948f30
Mevlut Arslan <MevlutArslan@users.noreply.github.com>
2021-02-09 05:31:28 +0100

    implemented learning rate decay

--- sgd_optimizer.py
+++ sgd_optimizer.py
@@ -1,9 +1,21 @@
 import numpy as np
 
+
 class SGD_Optimizer():
-    def __init__(self, learning_rate=1.0):
+    def __init__(self, learning_rate=1.0, decay=0.0):
         self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * \
+                (1 / (1 + self.decay * self.iterations))
 
     def update_parameters(self, layer):
         layer.weights += -self.learning_rate * layer.dweights
         layer.biases += -self.learning_rate * layer.dbiases
+
+    def post_update_params(self):
+        self.iterations += 1
