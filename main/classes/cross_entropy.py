dbef48347e0c6c33835a6d073480f3f7d96fcc1d
Mevlut Arslan <MevlutArslan@users.noreply.github.com>
2021-02-24 07:30:47 +0100

    implemented l1 and l2 regularization

--- main/classes/cross_entropy.py
+++ main/classes/cross_entropy.py
@@ -1,7 +1,6 @@
 import numpy as np
 from main.classes.loss_class import Loss
 
-
 class Loss_CategoricalCrossentropy(Loss):  # Forward pass
 
     def forward(self, y_pred, y_true):  # Number of samples in a batch
@@ -35,3 +34,24 @@ class Loss_CategoricalCrossentropy(Loss):  # Forward pass
         self.dinputs = -ground_truth_v / dvalues
 
         self.dinputs = self.dinputs / sample_count
+
+    def regularization_loss(self, layer):
+        regularization_loss = 0
+
+        if layer.weight_regularizer_l1 > 0 :
+            regularization_loss += layer.weight_regularizer_l1 + \
+                                    np.sum(np.abs(layer.weights))
+
+        if layer.weight_regularizer_l2 > 0:
+            regularization_loss += layer.weight_regularizer_l2 + \
+                                    np.sum(layer.weights * layer.weights)
+
+        if layer.bias_regularizer_l1 > 0:
+            regularization_loss += layer.bias_regularizer_l1 + \
+                                    np.sum(np.abs(layer.biases))
+
+        if layer.bias_regularizer_l2 > 0:
+            regularization_loss += layer.bias_regularizer_l2 + \
+                                    np.sum(layer.biases * layer.biases)
+
+        return regularization_loss
--- main/classes/layer_dense.py
+++ main/classes/layer_dense.py
@@ -6,11 +6,16 @@ nnfs.init()
 
 class Layer_Dense:
     # Layer initialization
-    def __init__(self, n_inputs, n_neurons):
+    def __init__(self, n_inputs, n_neurons, weight_regularizer_l1, 
+    weight_regularizer_l2, bias_regularizer_l1, bias_regularizer_l2):
         # Initialize weights and biases
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
-        # Forward pass
+        # regularization strength
+        self.weight_regularizer_l1 = weight_regularizer_l1
+        self.weight_regularizer_l2 = weight_regularizer_l2
+        self.bias_regularizer_l1 = bias_regularizer_l1
+        self.bias_regularizer_l2 = bias_regularizer_l2
 
     def forward(self, inputs):
         # we store cause : 
